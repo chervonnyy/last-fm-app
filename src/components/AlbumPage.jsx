3c241873c0a2c41158b50760ef7ca4becb59a79a
Vlad Chervonnyy <vchervonnyy@kameleoon.com>
2019-12-23 19:06:58 +0300

    getting artist data

--- src/components/AlbumPage.jsx
+++ src/components/AlbumPage.jsx
@@ -15,20 +15,35 @@ class AlbumPage extends Component {
         fetch(`http://ws.audioscrobbler.com/2.0/?method=album.getinfo&api_key=${apiKey}&artist=${artist}&album=${album}&format=json`)
             .then(res => res.json())
             .then(data => this.setState({ albumData: data.album }))
-            .catch(error => console.log(error));
+            .catch(error => console.error(error));
     }
     
     render() {
         const { artist, album } = this.props.location.state;
+        const apiKey = '7600702bed449a1234d7fe6d22c880a2';
         const albumData = this.state.albumData;
+
         console.log(albumData);
+        let style = { color: 'red' }
+    
+        if (albumData) {
+            fetch(`http://ws.audioscrobbler.com/2.0/?method=artist.getinfo&artist=${artist}&api_key=${apiKey}&format=json`)
+                .then(res => res.json())
+                .then(data => {
+                    const image = data.artist.image[4];
+                    style = {
+                        backgroundImage: `url(${image})`,
+                    }
+                })
+                .catch(error => console.error(error));
+        }
 
         return (
             <div className='album-page'>
                 <h2>{artist} — {album}</h2>
-                {this.state.albumData &&
+                {albumData &&
                     <div className='album-info'>
-                        <div className="album-info__main">
+                        <div className="album-info__main" stlye={{...style}}>
                             <div className='album-info__cover'>
                                 <img src={albumData.image[4]['#text']} alt={album} />
                             </div>
